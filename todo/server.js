516f35ef5c52395d5d3890672391cb2c0f166a5d
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-06-01 16:53:37 +0600

    understood how to send json data

--- todo/server.js
+++ todo/server.js
@@ -2,11 +2,41 @@ const http = require('http');
 const port = 5000;
 const host = 'localhost';
 
+const dummyData = [
+  {
+    id: 1,
+    title: 'First title',
+    description: 'lorem330dsgeetgfewgrtgesefsdfsdfgd',
+  },
+  {
+    id: 2,
+    title: 'Second title',
+    description: 'lorem330dsgeetgfewgrgesefsdfsdfgd',
+  },
+  {
+    id: 3,
+    title: 'Third title',
+    description: 'lorem330dsgeetgfewgrtgesefsdfsdfgd',
+  },
+  {
+    id: 4,
+    title: 'Fourth title',
+    description: 'lorem330dsgeetgfewgrtgsefsdfsdfgd',
+  },
+];
+
 const server = http.createServer((req, res) => {
   if (req.url === '/' && req.method === 'GET') {
-    res.end('Root route');
+    res.writeHead(200, {
+      'content-type': 'text/plain',
+    });
+    res.end('server is running...');
   } else if (req.url === '/todos' && req.method === 'GET') {
-    res.end('All todos');
+    res.writeHead(200, {
+      'content-type': 'application/json',
+      email: 'mehed@gmail.com',
+    });
+    res.end(JSON.stringify(dummyData));
   } else {
     res.end('Route not found');
   }
