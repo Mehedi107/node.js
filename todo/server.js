fec677d31b002779a3bc80957c75334895a1a6b4
Mehedi <mehedi.hsn.dev@gmail.com>
2026-06-28 16:52:44 +0600

    13-7 Routing in node js

--- todo/server.js
+++ todo/server.js
@@ -3,7 +3,16 @@ import http from 'http';
 const PORT = 5000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Node.js server");
+  // console.log("Request URL is: ", req.url, "Request method is: ",req.method);
+  // res.end("Hello from Node.js server");
+
+  if(req.url === '/todos' && req.method === 'GET') {
+    res.end("All todos here...");
+  } else if(req.url === '/todos/create-todo' && req.method === 'POST') {
+    res.end("Todo created");
+  }else {
+    res.end("Route not found")
+  }
 });
 
 server.listen(PORT, '127.0.0.1', () => {
